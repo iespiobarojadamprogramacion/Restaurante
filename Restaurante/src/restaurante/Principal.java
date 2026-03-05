b23cb5f0c74eafcbf088b1114655d10ab05d6eaf
vespertino <vespertino@ALUMNO08AULA27>
2026-03-05 19:28:45 +0100

    Incluido saludo Fernando

--- Restaurante/src/restaurante/Principal.java
+++ Restaurante/src/restaurante/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Soy David");
+		System.out.println("Soy Fernando");
 	}
 
 }
