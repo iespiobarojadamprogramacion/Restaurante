79960c3303596ad1a3c8b61142c29eebc21abc6c
programacion <programacion@programacion-VirtualBox>
2026-03-11 16:29:36 +0100

    He creado las clases clientes, reservas y el enum de reservas

---
+++ Restaurante/src/modelo/reservas.java
@@ -0,0 +1,29 @@
+package modelo;
+public class reservas {
+	 private static int contador = 1; 
+    private int idReserva;
+    private String fechaReserva;
+    private String horaReserva;
+    private int comensales;
+    private EstadoReservas estado;
+
+    public reservas(String fechaReserva, String horaReserva, int comensales, EstadoReservas estado) {
+    	this.idReserva = contador++; 
+        this.fechaReserva = fechaReserva;
+        this.horaReserva = horaReserva;
+        this.comensales = comensales;
+        this.estado = estado;
+    }
+
+    public void confirmar() {
+        estado = EstadoReservas.Confirmada;
+    }
+
+    public void cancelar() {
+        estado = EstadoReservas.Cancelada;
+    }
+
+    public boolean esParaFecha(String fecha) {
+        return this.fechaReserva.equals(fecha);
+    }
+}
