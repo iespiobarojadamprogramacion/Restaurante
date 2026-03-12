ac79f4c488a2f7306cfda1ff8793c493154b08a3
vespertino <vespertino@ALUMNO08AULA27>
2026-03-12 19:17:01 +0100

    Clases Todo Enums

--- Restaurante/src/restaurante/modelo/Plato.java
+++ Restaurante/src/restaurante/modelo/Plato.java
@@ -1,5 +1,28 @@
 package restaurante.modelo;
-
 public class Plato {
-
+    private int idPlato;
+    private String nombre;
+    private TipoPlato tipo;
+    private double precioUnitario;
+    private boolean disponibilidad;
+    // Constructor
+    public Plato(int idPlato, String nombre, TipoPlato tipo, double precioUnitario, boolean disponibilidad) {
+        this.idPlato = idPlato;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.precioUnitario = precioUnitario;
+        this.disponibilidad = disponibilidad;
+    }
+    public void cambiarDisponibilidad(boolean disponible) {
+        this.disponibilidad = disponible;
+    }
+    public void actualizarPrecio(double nuevoPrecio) {
+        this.precioUnitario = nuevoPrecio;
+    }
+    public double getPrecioUnitario() {
+        return precioUnitario;
+    }
+    public String getNombre() {
+        return nombre;
+    }
 }
