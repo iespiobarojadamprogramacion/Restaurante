469c8c20908a772787accbf3f310fbeb34fdaa3d
DanielTorresSanchez <daniel.torressanchez@educa.madrid.org>
2026-03-19 23:41:23 +0100

    Eliminar los nombres del main como dijo David

--- Restaurante/src/restaurante/Principal.java
+++ Restaurante/src/restaurante/Principal.java
@@ -4,11 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Soy David");
-		System.out.println("Soy Daniel Torres");
-    System.out.println("Soy Fernando");
-    System.out.println("Soy Brian");
-    	System.out.println("Soy Katterine");
+		
 	}
 
 }
