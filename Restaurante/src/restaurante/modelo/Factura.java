baa6fb4a98a318e29f7352ea6b9328d02b0aeab6
DanielTorresSanchez <daniel.torressanchez@educa.madrid.org>
2026-04-06 13:24:34 +0200

    Update Factura.java
    Versión espero que final de Factura

--- Restaurante/src/restaurante/modelo/Factura.java
+++ Restaurante/src/restaurante/modelo/Factura.java
@@ -1,63 +1,60 @@
 package restaurante.modelo;
 
 public class Factura {
+	private static int indice = 0;
     private int idFactura;
     private String fechaFactura;
-    private  double baseImponible;
-    private double impuesto;
     private MetodPago metodopago;
+    private int impuesto;
+    private Pedido pedido;
     
-    public Factura(int idFactura,String fechaFactura,double baseImponible,double impuesto,MetodPago metodopago) {
-    	this.idFactura=idFactura;
+    public Factura(String fechaFactura, MetodPago metodopago, int impuesto, Pedido pedido) {
+    	this.idFactura = indice++;
     	this.fechaFactura=fechaFactura;
-    	this.baseImponible=baseImponible;
     	this.metodopago=metodopago;
+    	this.pedido=pedido;
+    	this.impuesto = impuesto;
     }
 
 	public int getIdFactura() {
 		return idFactura;
 	}
 
-	public void setIdFactura(int idFactura) {
-		this.idFactura = idFactura;
-	}
-
 	public String getFechaFactura() {
 		return fechaFactura;
-	}
-
-	public void setFechaFactura(String fechaFactura) {
-		this.fechaFactura = fechaFactura;
-	}
-
-	public double getBaseImponible() {
-		return baseImponible;
-	}
-
-	public void setBaseImponible(double baseImponible) {
-		this.baseImponible = baseImponible;
 	}
 
 	public double getImpuesto() {
 		return impuesto;
 	}
 
-	public void setImpuesto(double impuesto) {
-		this.impuesto = impuesto;
-	}
-
 	public MetodPago getMetodopago() {
 		return metodopago;
 	}
 
-	public void setMetodopago(MetodPago metodopago) {
-		this.metodopago = metodopago;
+	public Pedido getPedido() {
+		return pedido;
+	}
+	
+	public double calcularBaseImponible() {
+		return pedido.calcularImporte();
+	}
+	
+	public double calcularImpuesto() {
+		return calcularBaseImponible() * (impuesto / 100.0);
+	}
+	
+	public double calcularTotal() {
+		return calcularBaseImponible() + calcularImpuesto();
 	}
 
-	@Override
-	public String toString() {
-		return "Factura [idFactura=" + idFactura + ", fechaFactura=" + fechaFactura + ", baseImponible=" + baseImponible
-				+ ", impuesto=" + impuesto + ", metodopago=" + metodopago + "]";
+	public String Imprimir() {
+		return "Factura [idFactura=" + idFactura + ", fechaFactura=" + fechaFactura + ", baseImponible=" + calcularBaseImponible()
+				+ ", impuesto=" + impuesto + ", metodopago=" + metodopago + ", pedido=" + pedido + "]";
 	}
-     
-}
+	
+	public void mostrarPedido() {
+		System.out.println(pedido);
+	}
+    
+    }
